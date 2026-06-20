5860af870131ed3379b5aa0b04a40cc7d4ee56c9
V17 <147525211+Fuyuv17@users.noreply.github.com>
2026-06-21 01:06:36 +0700

    Refactor to include function and improve interpolation

--- newtonTien.c
+++ newtonTien.c
@@ -1,26 +1,35 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<math.h>
+
+float f(float x) {
+	return pow(5, x)+cos(x);
+}
 
 int main () {
 	int n, i, j;
-	float X, s=1;
-	printf("Nhap n moc va X : "); scanf("%d%f", &n, &X);
-	float x[n], y[n];
+	float X, a=2, b=5;
 	
-	printf("Nhap bang ns : \n");
+	printf("Nhap so diem n va x* : "); 
+	scanf("%d%f", &n, &X);
+	
+	float h=(b-a)/(n-1);
+	float y[n];
+	
 	for(i=0;i<n;i++) {
-		scanf("%f%f", &x[i], &y[i]);
+		y[i]=f(a+i*h);
 	}
-	float h=x[1]-x[0];
-	float t=(X-x[0])/h;
-	float p=y[0];
-	for(j=1;j<n;j++) {
+
+	float t=(X-a)/h;
+	float p=y[0]; 
+	float s=1;    
+	
+	for(j=1; j<n; j++) {
 		for(i=0;i<n-j;i++) {
 			y[i]=y[i+1]-y[i];
 		}
 		s*=(t-j+1)/j;
 		p+=s*y[0];
 	}
-	printf("Gt can noi suy f(%f) : %f", X, p);
-	return 0;
+	printf("\nGia tri gan dung cua f(%.2f) la %f ", X, p);
 }
